7f88283703297e9902b2e8d7ef29c8e8ee3e8a5a
Alex Adams <alexadams2084@gmail.com>
2023-06-15 14:47:59 +0000

    Renamed last medal text to commendation

--- commands/commendations.py
+++ commands/commendations.py
@@ -15,7 +15,7 @@ def setup(bot):
 
 
 class Commendations(commands.Cog):
-    commendation_commands = discord.SlashCommandGroup("commendation", "Medal Commands")
+    commendation_commands = discord.SlashCommandGroup("commendation", "Commendation Commands")
     role_commands = commendation_commands.create_subgroup("role", "Role Commands")
 
     def __init__(self, bot:discord.Bot):
@@ -30,7 +30,7 @@ class Commendations(commands.Cog):
         db.create_commendation(title,description,emote)
         await ctx.respond(f"Created commendation: {title}")
 
-    class DeleteMedalView(discord.ui.View):
+    class DeleteCommendationView(discord.ui.View):
         def __init__(self, id, commendation, embed: discord.Embed):
             super().__init__()
             self.id = id
@@ -42,51 +42,51 @@ class Commendations(commands.Cog):
             db.delete_commendation(self.id)
             self.clear_items()
 
-            self.embed.title = "Deleted Medal"
+            self.embed.title = "Deleted Commendation"
             await interaction.response.edit_message(view=self, embed=self.embed)
 
-        @discord.ui.button(label="Keep Medal", style=discord.ButtonStyle.blurple) 
+        @discord.ui.button(label="Keep Commendation", style=discord.ButtonStyle.blurple) 
         async def second_button_callback(self, button, interaction):
             self.clear_items()
 
-            self.embed.title = "Kept Medal"
+            self.embed.title = "Kept Commendation"
             await interaction.response.edit_message(view=self, embed=self.embed)
     
     @commendation_commands.command(name="delete", description = "Delete a commendation from the database")
     async def delete(self, ctx: discord.ApplicationContext, 
-                     id: discord.Option(int, required=True, description='Medal ID')):
+                     id: discord.Option(int, required=True, description='Commendation ID')):
 
         commendation = db.get_commendation_info(id)
 
         embed = discord.Embed(
-            title=f"Delete Medal",
+            title=f"Delete Commendation",
             color=discord.Colour.from_rgb(255,0,0)
         )
         embed.add_field(name="Name",value=f"{commendation['Emote']} {commendation['Name']}")
         embed.add_field(name="Description",value=f"{commendation['Description']}")
 
-        await ctx.respond(embed=embed, view=self.DeleteMedalView(id, commendation, embed))
+        await ctx.respond(embed=embed, view=self.DeleteCommendationView(id, commendation, embed))
     
     @commendation_commands.command(name="award", description = "Award a commendation to player(s)")
-    async def awardMedal(self, ctx: discord.ApplicationContext, 
+    async def awardCommendation(self, ctx: discord.ApplicationContext, 
                          title: discord.Option(str,required=True, description='The title of the commendation'), 
                          emote: discord.Option(str,required=False, description='The emoji symbol for this commendation'), 
                          description: discord.Option(str,required=False, description='The commendation description')):
         pass
 
     @commendation_commands.command(name="unaward", description = "Unaward a commendation from player(s)")
-    async def unawardMedal(self, ctx: discord.ApplicationContext, 
+    async def unawardCommendation(self, ctx: discord.ApplicationContext, 
                            title: discord.Option(str,required=True, description='The title of the commendation'), 
                            emote: discord.Option(str,required=False, description='The emoji symbol for this commendation'), description: discord.Option(str,required=False, description='The commendation description')):
         pass
 
     @role_commands.command(name="assign", description = "Assign a role that will automatically be given to anyone who has this commendation")
-    async def assignRoleToMedal(self, ctx: discord.ApplicationContext, 
-                                id: discord.Option(int, required=True, description='Medal ID'), 
+    async def assignRoleToCommendation(self, ctx: discord.ApplicationContext, 
+                                id: discord.Option(int, required=True, description='Commendation ID'), 
                                 role: discord.Option(str,required=True, description='The role ID')):
         pass
 
     @role_commands.command(name="remove", description = "Remove the link between a role and a commendation")
-    async def removeRoleFromMedal(self, ctx: discord.ApplicationContext, 
-                                  id: discord.Option(int, required=True, description='Medal ID')):
+    async def removeRoleFromCommendation(self, ctx: discord.ApplicationContext, 
+                                  id: discord.Option(int, required=True, description='Commendation ID')):
         pass
