a89f0e65ab7b0de5c126f1f5af2fb26263c5e068
Alex Adams <alexadams2084@gmail.com>
2023-07-07 17:29:55 +0000

    Enable all cogs for dev build

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ intents.members = True
 intents.guilds = True
 bot = commands.Bot(intents=intents)
 
-# bot.load_extension('commands.progression')
+bot.load_extension('commands.progression')
 bot.load_extension('commands.join_manager')
-# bot.load_extension('commands.commendations')
-# bot.load_extension('commands.provinces')
+bot.load_extension('commands.commendations')
+bot.load_extension('commands.provinces')
 bot.load_extension('commands.role_manager')
-# bot.load_extension('api.webserver')
+bot.load_extension('api.webserver')
 
 bot.run(TOKEN)
